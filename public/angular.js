126816d88ad656e75d05f09f39d2e5442d05d04c
cwklausing <cwklausing@gmail.com>
2015-12-17 21:47:30 -0600

    Cleared files, setting up new file configuration

--- public/angular.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by cwklausing on 12/15/15.
- */
-var app = angular.module('app', []);
-app.controller("myController", ['$scope', '$http', function($scope, $http){
-    $scope.strings = [];
-}])
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by cwklausing on 12/15/15.
- */
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-router.get('/', function(req,res,next){
-    res.sendFile(path.join(__dirname, '../views/index.html'));
-})
