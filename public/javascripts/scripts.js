4668b43590da00749c69e1f39660ebb3490ee8fe
cwklausing <cwklausing@gmail.com>
2016-01-15 12:34:06 -0600

    showChord button click success! Successfully alters string values

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -4,11 +4,13 @@
 var app = angular.module('app', []);
 
 app.controller('converterCtrl', ['$scope', '$http', function($scope, $http){
-    //Creates empty guitar object
+
     $scope.menuStatus = "closed";
-    $scope.chordData = [];
-    $scope.chordData.rootSelect = null;
-    $scope.chordData.chordSelect = null;
+    $scope.chordData = {};
+    //Dropdown menus are initially set to
+    $scope.chordData.rootSelect = "rootSelect";
+    $scope.chordData.chordSelect = "chordSelect";
+    //Creates empty guitar object
     $scope.guitar = {};
     //Guitar initializes with all strings = -1
     $scope.guitar.string1 = -1;
@@ -18,20 +20,27 @@ app.controller('converterCtrl', ['$scope', '$http', function($scope, $http){
     $scope.guitar.string5 = -1;
     $scope.guitar.string6 = -1;
 
-    //Set string changes the string value, which triggers an active class
-    //In both the guitar and piano svg files. See classes .active, .keyactive,
-    //and .keyshadowactive in style.css
     $scope.showChord = function() {
         $http({
             method: 'GET',
             url: '/data/chords.json'
         }).then(function(res){
-            console.log(res.data);
-        }).catch(function(err){
-            console.log(err);
+            $scope.guitar.string1 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][0];
+            $scope.guitar.string2 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][1];
+            $scope.guitar.string3 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][2];
+            $scope.guitar.string4 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][3];
+            $scope.guitar.string5 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][4];
+            $scope.guitar.string6 = res.data[$scope.chordData.rootSelect][$scope.chordData.chordSelect][5];
+        },
+        function(res){
+            $scope.data = res.data || "Request failed";
+            $scope.status = res.status;
         });
     };
 
+    //Set string changes the string value, which triggers an active class
+    //In both the guitar and piano svg files. See classes .active, .keyactive,
+    //and .keyshadowactive in style.css
     $scope.setString = function(note, string){
         if($scope.guitar[string] === note) {
             $scope.guitar[string] = -1;
