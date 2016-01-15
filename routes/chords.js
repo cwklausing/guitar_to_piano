194a8f73ba0a813b6d72277514314d632255b278
cwklausing <cwklausing@gmail.com>
2016-01-15 12:31:48 -0600

    deleted unnecessary chords route

--- routes/chords.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET chords */
-router.get('/', function(req, res, next) {
-    res.send(req.body);
-});
-
-module.exports = router;
